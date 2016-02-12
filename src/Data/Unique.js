d73eca4b7f14233263664b9258d1e607aad8780d
mechairoi <ttsujikawa@gmail.com>
2016-02-12 22:40:13 +0900

    Add rules of jscs and jshint

--- src/Data/Unique.js
+++ src/Data/Unique.js
@@ -7,11 +7,11 @@ var MAX_SAFE_INTEGER = 9007199254740991;
 var uniqueSource = [];
 
 function iter(i) {
-  if(uniqueSource.length === i) {
-    for(var j = 0; j < i; ++j) uniqueSource[j] = 0;
+  if (uniqueSource.length === i) {
+    for (var j = 0; j < i; ++j) uniqueSource[j] = 0;
     uniqueSource[i] = 1;
   } else {
-    if(uniqueSource[i] === MAX_SAFE_INTEGER) {
+    if (uniqueSource[i] === MAX_SAFE_INTEGER) {
       uniqueSource[i] = 0;
       iter(i + 1);
     } else {
@@ -20,8 +20,8 @@ function iter(i) {
   }
 }
 
-exports.newUnique = function() {
+exports.newUnique = function () {
   iter(0);
-  return uniqueSource.length === 1 ? (uniqueSource[0] + "") : uniqueSource.join(',');
+  return uniqueSource.length === 1 ? uniqueSource[0] + "" : uniqueSource.join(",");
 };
 

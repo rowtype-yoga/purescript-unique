75f7cdbe8e8091d9472c3e3d14daf3ba31c732a5
mechairoi <ttsujikawa@gmail.com>
2016-02-13 00:21:13 +0900

    Refactoring tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,12 +1,15 @@
 module Test.Main where
 
-import Prelude
-import Data.Unique (newUnique)
-import Test.Assert (assert)
-import Control.Monad.Eff.Console (log)
+import Prelude ((==), ($), (/=), bind, Unit)
+import Data.Unique (newUnique, UNIQ)
+import Test.Assert (assert, ASSERT)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (log, CONSOLE)
 
+main :: forall e. Eff (console :: CONSOLE , uniq :: UNIQ , assert :: ASSERT | e) Unit
 main = do
   log "Test Data.Unique"
   a <- newUnique
   b <- newUnique
   assert $ a /= b
+  assert $ a == a
